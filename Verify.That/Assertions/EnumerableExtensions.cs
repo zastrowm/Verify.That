e45b8a8c9a8e152068eecbdceaf38ccbd07d514e
Mackenzie Zastrow <zastrowm+sourcecontrol@gmail.com>
2021-01-14 22:11:47 -0600

    Add expected length/count assertion method

--- Verify.That/Assertions/EnumerableExtensions.cs
+++ Verify.That/Assertions/EnumerableExtensions.cs
@@ -8,6 +8,68 @@ namespace VerifiedAssertions
   public static class EnumerableExtensions
   {
     /// <summary>
+    ///   Verifies that the enumerable has the given length or count.
+    /// </summary>
+    public static IVerificationTarget<T> ToHaveCount<T>(
+      this IVerificationTarget<T?> target,
+      int length,
+      FormattableString? message = null
+    )
+      where T : IEnumerable
+    {
+      return ToHaveLength(target, length, message);
+    }
+
+    /// <summary>
+    ///   Verifies that the enumerable has the given length or count.
+    /// </summary>
+    public static IVerificationTarget<T> ToHaveLength<T>(
+      this IVerificationTarget<T?> target,
+      int expectedLength,
+      FormattableString? message = null
+    )
+      where T : IEnumerable
+    {
+      return CallbackAssertion.Create<T>(
+        target!,
+        context =>
+        {
+          var writer = context.Writer;
+
+          if (target.Value == null)
+          {
+            context.WriteMessage(message);
+
+            using (writer.Indent())
+            {
+              writer.WriteLine($"Expected: == non-null enumerable with count/length of {expectedLength}");
+              writer.WriteLine($"But was:     {target}");
+            }
+
+            return false;
+          }
+
+          int actualCount = (target.Value as ICollection)?.Count ?? target.Value.OfType<object>().Count();
+
+          if (actualCount != expectedLength)
+          {
+            context.WriteMessage(message);
+
+            using (writer.Indent())
+            {
+              writer.WriteLine($"Expected count: {expectedLength}");
+              writer.WriteLine($"But was:        {actualCount}");
+            }
+
+            return false;
+          }
+
+          return true;
+        }
+      );
+    }
+
+    /// <summary>
     ///   Verifies that the given IEnumerable equals another IEnumerable, element-wise
     /// </summary>
     public static IVerificationTarget<T> ToSequenceEqual<T>(
